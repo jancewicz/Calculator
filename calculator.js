312621d1e2afda5a22bf919b7ec9163fa93fb263
jancewicz <jancewicz.pawel1@gmail.com>
2024-01-11 23:11:45 +0100

    power on and cancel button works, starting to work on saving the numbers by clicking

--- calculator.js
+++ calculator.js
@@ -1,14 +1,38 @@
+// work with this let currentValue = 0; as starting point, then current value is updated and displayed on screen! with innerText
+// check function - window.onload
+let currentValue = 0;
+
+
+function setDisplayToZero() {
+    currentValue = 0;
+    document.getElementById("displayScreen").querySelector("span").textContent = currentValue;
+}
+
+function powerButtonSound() {
+    let sound = new Audio("enable-device-sound.mp3");
+    sound.play();
+}
+
+function turnCalculatorOn() {
+    powerButtonSound();
+    setDisplayToZero();
+}
+
+function powerButton() {
+    const powerButton = document.getElementById("pwr");
+    powerButton.addEventListener("click", turnCalculatorOn);
+}
+
+//find difference in these two functions
+function updateScreenDisplay() {
+    document.getElementById("screen").innerText = currentValue;
+}
+
+
 function savePressedKey(e) {
     const pressedKey = e.target.id;
     return pressedKey
 
-}
-
-function displayOnScreen(numericButtonClicked) {
-    const screenSpan = document.getElementById("displayScreen").querySelector("span");
-    let newSpan = document.createElement("span");
-    newSpan.textContent = numericButtonClicked;
-    screenSpan.appendChild(newSpan);
 }
 
 function numericButtons() {
@@ -17,16 +41,12 @@ function numericButtons() {
     buttons.forEach(button => {
         button.addEventListener("click", function (e) {
             numericButtonClicked = savePressedKey(e);
-            numericButtonClickedINT = parseInt(numericButtonClicked);
-            console.log(numericButtonClickedINT);
-            // below code allows to not append to starting 0 like after pressing 1 it's 01, but change it from single 0 to 1,
-            // but it changes the number every time
-            document.getElementById("displayScreen").querySelector("span").textContent = displayOnScreen(numericButtonClicked);
-            displayOnScreen(numericButtonClicked);
-
+            numericButtonClicked = parseInt(numericButtonClicked);
+            console.log(numericButtonClicked);
+            currentValue += numericButtonClicked;
+            updateScreenDisplay(currentValue);
         });
     });
-    return numericButtonClicked;
 }
 
 function functionalButtons() {
@@ -41,14 +61,12 @@ function functionalButtons() {
     return functionalButtonClicked;
 }
 
-function setDisplayToZero() {
-    document.getElementById("displayScreen").querySelector("span").textContent = 0;
-}
-
 function cancelButton() {
     const cancelButton = document.getElementById("cancel");
     cancelButton.addEventListener("click", setDisplayToZero);
 }
+
+
 
 
 // Math
@@ -76,7 +94,12 @@ function percent(num) {
     return num / 100;
 }
 
-let numericButtonClicked = numericButtons();
+
+
+
+powerButton(currentValue);
+numericButtons(currentValue);
 let functionalButtonClicked = functionalButtons();
 cancelButton();
-//displayOnScreen(numericButtonClicked);
+
+
